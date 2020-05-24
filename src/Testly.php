34d44469f80e687df5af87722840930be2a7a0c2
Larzs <wester@inbox.lv>
2020-05-25 01:55:17 +0300

    Say hello from Testly

---
+++ src/Testly.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Crudly\Testly;
+
+class Testly
+{
+    /**
+     * Say hello from package.
+     *
+     */
+    public static function hello() {
+        return 'Hello, world!';
+    }
+}
